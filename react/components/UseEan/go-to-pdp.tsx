408caa83182662660be0de3e8f59c9b6653d65c4
Your Name <bonacchigerman@gmail.com>
2021-09-28 16:40:55 -0300

    0.6.2

--- react/components/UseEan/go-to-pdp.tsx
+++ react/components/UseEan/go-to-pdp.tsx
@@ -6,10 +6,7 @@ import { useLazyQuery } from 'react-apollo'
 // eslint-disable-next-line prettier/prettier
 import type {
   MessageDescriptor} from 'react-intl';
-import {
-  useIntl,
-  defineMessages,
-} from 'react-intl'
+import { useIntl, defineMessages } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
 import type { ModalType, UseEanProps, SkuDataType } from '../../typings/global'
@@ -19,10 +16,21 @@ import findSkuOfMultipleEan from '../../utils/findSkuOfMultipleEan'
 
 import '../../style/Loading.global.css'
 
-const CSS_HANDLES = ['modalReaderMessagesError','modalReaderMessagesErrorText','modalReaderMessagesSucces','modalReaderMessagesSuccesText']
+const CSS_HANDLES = [
+  'modalReaderMessagesError',
+  'modalReaderMessagesErrorText',
+  'modalReaderMessagesSucces',
+  'modalReaderMessagesSuccesText',
+  'listErrorMutipleProductText',
+]
 
-export default function UseEanGoToPDP({setButton, setUse, ean, type, mode}: UseEanProps) {
-
+export default function UseEanGoToPDP({
+  setButton,
+  setUse,
+  ean,
+  type,
+  mode,
+}: UseEanProps) {
   const [skuData, setSkuData] = useState<SkuDataType>()
   const [isRedirect, setIsRedirect] = useState<boolean>(false)
 
@@ -30,8 +38,19 @@ export default function UseEanGoToPDP({setButton, setUse, ean, type, mode}: UseE
   const [messageModal, setMessageModal] = useState<string>('')
   const [modalType, setModalType] = useState<ModalType>()
 
-  const [getSkuQuery,{ loading: loadingGetSku, error: errorGetSku, data: dataGetSku }] = useLazyQuery(getDataSku)
-  const [getProductQuery,{ loading: loadingGetProduct, error: errorGetProduct, data: dataGetProduct }] = useLazyQuery(getProduct)
+  const [
+    getSkuQuery,
+    { loading: loadingGetSku, error: errorGetSku, data: dataGetSku },
+  ] = useLazyQuery(getDataSku)
+
+  const [
+    getProductQuery,
+    {
+      loading: loadingGetProduct,
+      error: errorGetProduct,
+      data: dataGetProduct,
+    },
+  ] = useLazyQuery(getProduct)
 
   const handles = useCssHandles(CSS_HANDLES)
 
@@ -39,24 +58,24 @@ export default function UseEanGoToPDP({setButton, setUse, ean, type, mode}: UseE
 
   let messagesInternationalization: any
 
-  if (type === 'qr'){
-     messagesInternationalization = defineMessages({
-        messageModalError: { id: 'store/qr-reader.messageModalError' },
-        messageModalSucces: { id: 'store/reader.messageModalSucces' },
-        retry: { id: 'store/reader.retry' },
-        cancel: { id: 'store/reader.cancel' },
-      })
-  }else if (type === 'barcode'){
-     messagesInternationalization = defineMessages({
-        messageModalError: { id: 'store/barcode-reader.messageModalError' },
-        messageModalSucces: { id: 'store/reader.messageModalSucces' },
-        retry: { id: 'store/reader.retry' },
-        cancel: { id: 'store/reader.cancel' },
-      })
-    }
+  if (type === 'qr') {
+    messagesInternationalization = defineMessages({
+      messageModalError: { id: 'store/qr-reader.messageModalError' },
+      messageModalSucces: { id: 'store/reader.messageModalSucces' },
+      retry: { id: 'store/reader.retry' },
+      cancel: { id: 'store/reader.cancel' },
+    })
+  } else if (type === 'barcode') {
+    messagesInternationalization = defineMessages({
+      messageModalError: { id: 'store/barcode-reader.messageModalError' },
+      messageModalSucces: { id: 'store/reader.messageModalSucces' },
+      retry: { id: 'store/reader.retry' },
+      cancel: { id: 'store/reader.cancel' },
+    })
+  }
 
   const translateMessage = (message: MessageDescriptor) =>
-  intl.formatMessage(message)
+    intl.formatMessage(message)
 
   const openModalResult = () => {
     setModalResult(true)
@@ -65,147 +84,181 @@ export default function UseEanGoToPDP({setButton, setUse, ean, type, mode}: UseE
   const closeModalResult = () => {
     setModalResult(false)
   }
-  
+
   useEffect(() => {
     const queryParam = ean
 
     getSkuQuery({ variables: { ean: queryParam } })
   }, [])
 
-  useEffect ( () => {
-    if(!loadingGetSku && !errorGetSku && !dataGetSku ) return
-    if(loadingGetSku){
+  useEffect(() => {
+    if (!loadingGetSku && !errorGetSku && !dataGetSku) return
+    if (loadingGetSku) {
       setMessageModal(``)
       openModalResult()
     }
 
-    if(errorGetSku){
-      if (mode === 'singleEan'){
-        setMessageModal(`${translateMessage(messagesInternationalization.messageModalError)}`)
+    if (errorGetSku) {
+      if (mode === 'singleEan') {
+        setMessageModal(
+          `${translateMessage(messagesInternationalization.messageModalError)}`
+        )
         setModalType('error')
-      } else if (mode === 'multipleEan'){
+      } else if (mode === 'multipleEan') {
         const queryParam = ean
 
         getProductQuery({ variables: { ean: queryParam } })
       }
     }
 
-    if(dataGetSku){
+    if (dataGetSku) {
       const sku: SkuDataType = dataGetSku.getSku.data
       const productName: string = sku.NameComplete
 
-      setMessageModal(`${translateMessage(messagesInternationalization.messageModalSucces)} ${productName}`)
+      setMessageModal(
+        `${translateMessage(
+          messagesInternationalization.messageModalSucces
+        )} ${productName}`
+      )
       setModalType('success')
       setSkuData(sku)
-    }else{
+    } else {
       null
     }
-
-  },[loadingGetSku,errorGetSku,dataGetSku]
-  )
-
+  }, [loadingGetSku, errorGetSku, dataGetSku])
 
   useEffect(() => {
-    if(!loadingGetProduct && !errorGetProduct && !dataGetProduct ) return
-    if(loadingGetProduct){
+    if (!loadingGetProduct && !errorGetProduct && !dataGetProduct) return
+    if (loadingGetProduct) {
       setMessageModal(``)
       openModalResult()
     }
 
-    if(errorGetProduct){
-        setMessageModal(`${translateMessage(messagesInternationalization.messageModalError)}`)
-        setModalType('error')
+    if (errorGetProduct) {
+      setMessageModal(
+        `${translateMessage(messagesInternationalization.messageModalError)}`
+      )
+      setModalType('error')
     }
 
-    if(dataGetProduct){
-      const {data} = dataGetProduct.getProductBySpecificationFilter
+    if (dataGetProduct) {
+      const { data } = dataGetProduct.getProductBySpecificationFilter
 
-      if (data.length>0){
-
+      if (data.length > 0) {
         const [{ MultipleEan, linkText, items }] = data
 
-        if(MultipleEan){
+        if (MultipleEan) {
           const skuFinded = findSkuOfMultipleEan(MultipleEan, ean)
 
-          const { nameComplete } = items.find((item) => item.itemId === skuFinded)
-          
+          const { nameComplete } = items.find(
+            (item) => item.itemId === skuFinded
+          )
+
           const skuTemp: SkuDataType = {
             Id: skuFinded,
             NameComplete: nameComplete,
-            DetailUrl: `${linkText}/p`
+            DetailUrl: `${linkText}/p`,
           }
-  
-          setMessageModal(`${translateMessage(messagesInternationalization.messageModalSucces)} ${nameComplete}`)
+
+          setMessageModal(
+            `${translateMessage(
+              messagesInternationalization.messageModalSucces
+            )} ${nameComplete}`
+          )
           setModalType('success')
           setSkuData(skuTemp)
-        }else{
-        setMessageModal(`${translateMessage(messagesInternationalization.messageModalError)}`)
+        } else {
+          setMessageModal(
+            `${translateMessage(
+              messagesInternationalization.messageModalError
+            )}`
+          )
+          setModalType('error')
+        }
+      } else {
+        setMessageModal(
+          `${translateMessage(messagesInternationalization.messageModalError)}`
+        )
         setModalType('error')
-     }
-        
-     }else{
-        setMessageModal(`${translateMessage(messagesInternationalization.messageModalError)}`)
-        setModalType('error')
-     }
+      }
     }
   }, [loadingGetProduct, errorGetProduct, dataGetProduct])
 
-
   useEffect(() => {
-    if(!skuData) return
+    if (!skuData) return
 
     // eslint-disable-next-line vtex/prefer-early-return
-    if (!isRedirect){
+    if (!isRedirect) {
       const skuLink = `${skuData.DetailUrl}?skuId=${skuData.Id}`
 
       setIsRedirect(true)
       window.location.replace(skuLink)
     }
-
   }, [skuData])
-
-
 
   return (
     <div>
-      {modalType === 'error' && 
-      <ModalDialog
-        centered
-        isOpen={modalResult}
-        confirmation={{
-          label: translateMessage(messagesInternationalization.retry),
-          onClick: () => {
+      {modalType === 'error' && (
+        <ModalDialog
+          centered
+          isOpen={modalResult}
+          confirmation={{
+            label: translateMessage(messagesInternationalization.retry),
+            onClick: () => {
+              closeModalResult()
+              setUse(false)
+              setTimeout(() => {
+                setUse(true)
+              }, 1000)
+            },
+          }}
+          cancelation={{
+            onClick: () => {
+              closeModalResult()
+              setButton(false)
+            },
+            label: translateMessage(messagesInternationalization.cancel),
+          }}
+          onClose={() => {
             closeModalResult()
-            setUse(false)
-            setTimeout(() => {
-              setUse(true)
-            }, 1000);
-          },
-        }}
-        cancelation={{
-          onClick: () => {
-            closeModalResult() 
-            setButton(false)
-          },
-          label: translateMessage(messagesInternationalization.cancel),
-        }}
-        onClose={() => {closeModalResult()}}>
-        <div className={`${handles.modalReaderMessagesError}`}>
-          <span className={`${handles.modalReaderMessagesErrorText} f3 f3-ns fw3 gray c-action-primary fw5`}> {messageModal} </span>
-          {(isRedirect || messageModal === '') && <div className="loading-container"><Spinner /></div>}
-        </div>
-      </ModalDialog>}
-      {modalType === 'success' && 
-      <Modal
-        centered
-        isOpen={modalResult}
-        onClose={() => {closeModalResult()}}>
-        <div className={`${handles.modalReaderMessagesSucces}`}>
-          <span className={`${handles.modalReaderMessagesSuccesText} f3 f3-ns fw3 gray c-action-primary fw5`}> {messageModal} </span>
-          {(isRedirect || messageModal === '') && <div className="loading-container"><Spinner /></div>}
-        </div>
-      </Modal>}
-    </div> 
+          }}
+        >
+          <div className={`${handles.modalReaderMessagesError}`}>
+            <span
+              className={`${handles.modalReaderMessagesErrorText} f3 f3-ns fw3 gray c-action-primary fw5`}
+            >
+              {messageModal}
+            </span>
+            {(isRedirect || messageModal === '') && (
+              <div className="loading-container">
+                <Spinner />
+              </div>
+            )}
+          </div>
+        </ModalDialog>
+      )}
+      {modalType === 'success' && (
+        <Modal
+          centered
+          isOpen={modalResult}
+          onClose={() => {
+            closeModalResult()
+          }}
+        >
+          <div className={`${handles.modalReaderMessagesSucces}`}>
+            <span
+              className={`${handles.modalReaderMessagesSuccesText} f3 f3-ns fw3 gray c-action-primary fw5`}
+            >
+              {messageModal}
+            </span>
+            {(isRedirect || messageModal === '') && (
+              <div className="loading-container">
+                <Spinner />
+              </div>
+            )}
+          </div>
+        </Modal>
+      )}
+    </div>
   )
 }
-
