21ade83d0fd486446b61b00f34cf8218ac69cc75
Your Name <bonacchigerman@gmail.com>
2021-12-16 11:27:12 -0300

    yarn lint

--- react/components/BarcodeReaderWrapper.tsx
+++ react/components/BarcodeReaderWrapper.tsx
@@ -10,47 +10,46 @@ import BarcodeContainer from './barcode-scanner'
 
 const CSS_HANDLES = ['barcodeReaderWrapper', 'barcodeReaderButton']
 
-const BarcodeReaderWrapper: StorefrontFunctionComponent<BarcodeReaderWrapperProps> =
-  ({ action, mode }) => {
-    const [readBarcode, setReadBarcode] = useState<boolean>(false)
+const BarcodeReaderWrapper: StorefrontFunctionComponent<
+  BarcodeReaderWrapperProps
+> = ({ action, mode }) => {
+  const [readBarcode, setReadBarcode] = useState<boolean>(false)
 
-    const intl = useIntl()
-    const searchIcon = <Search />
-    const messagesInternationalization = defineMessages({
-      buttonOpenReader: { id: 'store/barcode-reader.buttonOpenReaderBarcode' },
-    })
+  const intl = useIntl()
+  const searchIcon = <Search />
+  const messagesInternationalization = defineMessages({
+    buttonOpenReader: { id: 'store/barcode-reader.buttonOpenReaderBarcode' },
+  })
 
-    const translateMessage = (message: MessageDescriptor) =>
-      intl.formatMessage(message)
+  const translateMessage = (message: MessageDescriptor) =>
+    intl.formatMessage(message)
 
-    const onclickBarcodeReader = () => {
-      setReadBarcode(!readBarcode)
-    }
-
-    const handles = useCssHandles(CSS_HANDLES)
-
-    return (
-      <div className={`${handles.barcodeReaderWrapper} c-muted-1 db tc`}>
-        <div className={`${handles.barcodeReaderButton} mb2`}>
-          <ButtonWithIcon
-            icon={searchIcon}
-            variation="primary"
-            onClick={onclickBarcodeReader}
-          >
-            {`${translateMessage(
-              messagesInternationalization.buttonOpenReader
-            )}`}
-          </ButtonWithIcon>
-        </div>
-        {readBarcode && (
-          <BarcodeContainer
-            setButtonUseBarcode={setReadBarcode}
-            action={action}
-            mode={mode}
-          />
-        )}
-      </div>
-    )
+  const onclickBarcodeReader = () => {
+    setReadBarcode(!readBarcode)
   }
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  return (
+    <div className={`${handles.barcodeReaderWrapper} c-muted-1 db tc`}>
+      <div className={`${handles.barcodeReaderButton} mb2`}>
+        <ButtonWithIcon
+          icon={searchIcon}
+          variation="primary"
+          onClick={onclickBarcodeReader}
+        >
+          {`${translateMessage(messagesInternationalization.buttonOpenReader)}`}
+        </ButtonWithIcon>
+      </div>
+      {readBarcode && (
+        <BarcodeContainer
+          setButtonUseBarcode={setReadBarcode}
+          action={action}
+          mode={mode}
+        />
+      )}
+    </div>
+  )
+}
 
 export default BarcodeReaderWrapper
