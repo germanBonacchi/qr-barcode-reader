c33d08c9ce3b88e80d6bb91ba28e22c82e200904
Your Name <bonacchigerman@gmail.com>
2022-05-05 14:35:07 -0300

    remove console.logs

--- node/resolvers/catalogSystem/index.ts
+++ node/resolvers/catalogSystem/index.ts
@@ -1,11 +1,8 @@
 export const queries = {
   getSku: async (_: unknown, { ean }: QueryParamEan, ctx: Context) => {
     try {
-      console.info('getSku', ean)
-
       return await ctx.clients.skuByEan.getSku(ean)
     } catch (error) {
-      console.error('error getSku', error)
       throw new Error('No sku was found.')
     }
   },
